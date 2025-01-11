38332eda65f3dfd2f4bb0ce7e4b33f9af44b64ac
kblaszki <blaszkiewicz121@protonmail.com>
2025-01-11 20:07:29 +0100

    Create User model.

---
+++ server/src/models/User.js
@@ -0,0 +1,17 @@
+import {DataTypes} from 'sequelize';
+
+import {define} from '../config/database';
+
+const User = define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default User;
