c96032b0a4450498205f11167675eea3e6cf44de
kblaszki <blaszkiewicz121@protonmail.com>
2025-01-12 13:37:46 +0100

    Add auth routes.

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import express from 'express';
 
 import sequelize from './config/database.js';
+import authRoutes from './routes/auth.js';
 
 sequelize.sync().then(() => {
   console.log('Database synced!');
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api/auth/', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('HabitFlow API is running!');
